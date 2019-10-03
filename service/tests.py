51c8e34deeae09910da6f5fcd0354ab1b416b68c
Max G <maxdotdotg@gmail.com>
2019-10-02 23:37:39 -0400

    rename testcases, add positive test case

--- service/tests.py
+++ service/tests.py
@@ -6,21 +6,21 @@ import is_flappy
 
 class Test_service(unittest.TestCase):
 
-    def test1(self):
+    def test_service_is_running(self):
         # service running on localhost should return 200
         test_client = is_flappy.app.test_client()
 
         r = test_client.get("/check/localhost")
         self.assertEqual(r.status_code, 200)
 
-    def test2(self):
+    def test_service_returns_json(self):
         # return type should always be json
         test_client = is_flappy.app.test_client()
         
         r = test_client.get("/check/localhost")
         self.assertEqual(r.is_json, True)
 
-    def test3(self):
+    def test_check_not_running_service(self):
         # check a service that is not running
         test_client = is_flappy.app.test_client()
         payload = {"port": 9001, "hosts": ["localhost"]}
@@ -31,3 +31,16 @@ class Test_service(unittest.TestCase):
             content_type="application/json"
         )
         self.assertEqual(r.json["results"][0]["status"], "DOWN")
+
+    def test_check_running_service(self):
+        # test a service that IS running
+        test_client = is_flappy.app.test_client()
+        payload = {"port": 80, "hosts": ["www.google.com"]}
+
+        r = test_client.post(
+            "/json", 
+            data=dumps(payload),
+            content_type="application/json"
+        )
+        self.assertEqual(r.json["results"][0]["status"], "OK")
+
